005bd6f2368307d8c6406b1970143e5533b4bd5e
agu$ <agustin.vigelal.06@gmail.com>
2023-11-07 20:01:11 -0300

    first commit

---
+++ practica.js
@@ -0,0 +1,6 @@
+const eliminar = document.querySelector(".eliminar")
+const contenedor = document.querySelector(".contenedor-1")
+
+eliminar.addEventListener("click", function(){
+    contenedor.remove()
+})
